f336b61300a3e6a812950ea1ecfa373852ba5840
“name“omar <121135630+OmarAbd-Almaksoud@users.noreply.github.com>
2024-03-23 19:59:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
 import { useState } from "react";
-
 import "./App.css";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { AppProvider } from "./Contexts/appContext";
-// import { BrowserRouter, Route, Router, Routes, createBrowserRouter } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
-import HomePage from "./Pages/HomePage"
 import ProtectedRoutes from "./Guard/authGuard";
-
-
+import Home from "./Pages/Home/home";
 
 function App() {
   const [count, setCount] = useState(0);
-  
 
   return (
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage/>}></Route>
-          <Route path="/register" element={<RegisterPage/>}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
 
           {/* protected routes  */}
           <Route element={<ProtectedRoutes />}>
-          <Route path="/home" element={<HomePage/>}></Route>
+            <Route path="/home" element={<Home />}></Route>
           </Route>
-
         </Routes>
       </BrowserRouter>
-     
- </AppProvider>
+    </AppProvider>
   );
 }
 
