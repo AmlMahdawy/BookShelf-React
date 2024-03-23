dd7392f2cdc13b818f686d583be41ffe7fbf2778
Aml <amlmahdawy21@gmail.com>
2024-03-23 06:30:02 +0200

    setting context and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import { AppProvider } from "./Contexts/appContext";
+import { BrowserRouter, Route, Router, Routes, createBrowserRouter } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+import HomePage from "./Pages/HomePage"
+import ProtectedRoutes from "./Guard/authGuard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/register" element={<RegisterPage/>}></Route>
+
+          {/* protected routes  */}
+          <Route element={<ProtectedRoutes />}>
+          <Route path="/home" element={<HomePage/>}></Route>
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
+     
+ </AppProvider>
+  );
 }
 
-export default App
+export default App;
