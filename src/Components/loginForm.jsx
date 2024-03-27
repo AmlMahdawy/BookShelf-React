027d48982ebda3733602693efc8c52ea17d04d57
Aml <amlmahdawy21@gmail.com>
2024-03-27 22:59:55 +0200

    login design

--- src/Components/loginForm.jsx
+++ src/Components/loginForm.jsx
@@ -1,50 +1,99 @@
+import { GoogleLogin } from '@react-oauth/google';
+import {jwtDecode} from 'jwt-decode';
+
 import { useCallback, useState } from "react";
 import { useApp } from "../Contexts/appContext";
-import "../Styles/LoginForm.module.css";
+import styles from "../Styles/LoginForm.module.css";
 import { useNavigate } from "react-router-dom";
+import { CleaningServices } from '@mui/icons-material';
 
 const LoginForm = () => {
   const { login } = useApp();
   const navigate = useNavigate();
 
   const [userData, setUserData] = useState({ email: "", password: "" });
+  const [emailError, setEmailError] = useState("");
 
-  async function hanleLoginClick(event) {
+  const handleLoginClick = async (event) => {
     event.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userData.email)) {
+      setEmailError("Please enter a valid email address.");
+      return; 
+    } else {
+      setEmailError("");
+    }
     let res = await login(userData);
+   
     if (res) {
       navigate("/home");
- 
+    }else{
+      setEmailError("Invalid Email or Password");
+
     }
-  }
-  function handleOnChange(event) {
+  };
+
+  const handleOnChange = (event) => {
     setUserData((oldUser) => ({
       ...oldUser,
       [event.target.name]: event.target.value,
     }));
-  }
+  };
 
   return (
     <>
-      <form>
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={handleOnChange}
-        ></input>
-        <br></br>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          onChange={handleOnChange}
-        ></input>
-        <br></br>
-        <button onClick={hanleLoginClick}>Login</button>
-      </form>
+        <div className={styles.overLay}></div>
+
+      <div className={styles.mainContainer}>
+        <div className={styles.imgContainer}>
+        
+        </div>
+
+        <form autoComplete="off">
+          <div className={styles.logo}>
+            <span> Welcome Back</span></div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="email">E-mail:</label>
+        
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email address"
+              onChange={handleOnChange}
+            />
+           
+          </div>
+
+          <div className={styles.inputContainer}>
+            <label htmlFor="password">Password:</label>
+            
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Your password"
+              onChange={handleOnChange}
+            />
+          </div>
+
+        
+          {emailError && <div className={styles.error}>{emailError}</div>}
+            <button onClick={handleLoginClick}>Login</button>
+
+<GoogleLogin
+  onSuccess={credentialResponse => {
+   console.log( jwtDecode(credentialResponse.credential,"hi"))
+    
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+  className={styles.googleBtn}
+/>;
+      
+        </form>
+      </div>
     </>
   );
 };
