39272e408f3fd9aed814e502a8cee07edbc09458
Aml <amlmahdawy21@gmail.com>
2024-03-28 08:51:02 +0200

    login with credentials , gmail login ,design

--- src/Components/loginForm.jsx
+++ src/Components/loginForm.jsx
@@ -1,35 +1,32 @@
-import { GoogleLogin } from '@react-oauth/google';
-import {jwtDecode} from 'jwt-decode';
-
-import { useCallback, useState } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
 import { useApp } from "../Contexts/appContext";
 import styles from "../Styles/LoginForm.module.css";
 import { useNavigate } from "react-router-dom";
-import { CleaningServices } from '@mui/icons-material';
 
 const LoginForm = () => {
+
   const { login } = useApp();
   const navigate = useNavigate();
 
-  const [userData, setUserData] = useState({ email: "", password: "" });
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    gmail: true,
+  });
+
   const [emailError, setEmailError] = useState("");
 
   const handleLoginClick = async (event) => {
-    event.preventDefault();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(userData.email)) {
-      setEmailError("Please enter a valid email address.");
-      return; 
-    } else {
-      setEmailError("");
+    if (event) {
+      event.preventDefault();
     }
     let res = await login(userData);
-   
     if (res) {
       navigate("/home");
-    }else{
+    } else {
       setEmailError("Invalid Email or Password");
-
     }
   };
 
@@ -42,57 +39,69 @@ const LoginForm = () => {
 
   return (
     <>
+      <div className={styles.container}>
         <div className={styles.overLay}></div>
 
-      <div className={styles.mainContainer}>
-        <div className={styles.imgContainer}>
-        
+        <div className={styles.mainContainer}>
+
+          <div className={styles.imgContainer}></div>
+
+          <form autoComplete="off" className={styles.loginForm}>
+
+            <div className={styles.welcomeMsg}>
+              <span> Welcome Back</span>
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label htmlFor="email" className={styles.Labels}>
+                E-mail:
+              </label>
+              <input
+                className={styles.inputs}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your Email address"
+                onChange={handleOnChange}
+              />
+            </div>
+
+            <div className={styles.inputContainer}>
+              <label htmlFor="password" className={styles.Labels}>
+                Password:
+              </label>
+              <input
+                className={styles.inputs}
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Your password"
+                onChange={handleOnChange}
+              />
+            </div>
+
+            {emailError && <div className={styles.error}>{emailError}</div>}
+
+            <button onClick={handleLoginClick} className={styles.btnLogin}>
+              Login
+            </button>
+            <GoogleLogin
+              onSuccess={async (credentialResponse) => {
+                let mail = await jwtDecode(credentialResponse.credential).email;
+                setUserData((oldUser) => ({
+                  ...oldUser,
+                  gmail: true,
+                  email: mail,
+                }));
+                handleLoginClick();
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+            ;
+          </form>
         </div>
-
-        <form autoComplete="off">
-          <div className={styles.logo}>
-            <span> Welcome Back</span></div>
-          <div className={styles.inputContainer}>
-            <label htmlFor="email">E-mail:</label>
-        
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Your Email address"
-              onChange={handleOnChange}
-            />
-           
-          </div>
-
-          <div className={styles.inputContainer}>
-            <label htmlFor="password">Password:</label>
-            
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Your password"
-              onChange={handleOnChange}
-            />
-          </div>
-
-        
-          {emailError && <div className={styles.error}>{emailError}</div>}
-            <button onClick={handleLoginClick}>Login</button>
-
-<GoogleLogin
-  onSuccess={credentialResponse => {
-   console.log( jwtDecode(credentialResponse.credential,"hi"))
-    
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-  className={styles.googleBtn}
-/>;
-      
-        </form>
       </div>
     </>
   );
