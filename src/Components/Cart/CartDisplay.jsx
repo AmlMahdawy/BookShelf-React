d019edb9009060a0d3c403a9fe0181822943ff2b
Alaa Anees <alaaaneessaad@gmail.com>
2024-03-29 01:46:28 +0200

    css file cleaned

--- src/Components/Cart/CartDisplay.jsx
+++ src/Components/Cart/CartDisplay.jsx
@@ -85,6 +85,7 @@ export default function CartDisplay() {
       <div className="my-5">
         {cartItems.length === 0 ? (
           <>
+            {/*empty cart start*/}
             <div className=" container">
               <div className="empty-cart">
                 <div className="empty-cart-text">
@@ -97,20 +98,26 @@ export default function CartDisplay() {
                   </h5>
                 </div>
               </div>
+              {/*empty cart end*/}
             </div>
+            {/* return to shop btn start */}
             <div className="return-to-shop my-5">
               <button className="return-to-shop-btn">Return To Shop</button>
             </div>
+            {/* return to shop btn end */}
           </>
         ) : (
+          /* cart wrapper start */
           <div className="cart-wrapper container">
             {cartItems.map((item, index) => (
+              /* row 1 start */
               <div
                 className="row align-items-center mb-4 item-wrapper"
                 key={index}
               >
                 <div className="col-lg-5 ">
                   <div className="img-info-wrapper d-flex align-items-center">
+                    {/* book img start */}
                     <div className="img-wrapper hvr-curl-top-right">
                       <img
                         src={item.book.thumbnailUrl}
@@ -118,17 +125,21 @@ export default function CartDisplay() {
                         className="item-img  "
                       />
                     </div>
-                    <div className=" ">
-                      <div className="item-info">
-                        <span className="isbn">ISBN {item.book.isbn}</span>
-                        <h5 className="book-title">{item.book.title}</h5>
-                        <span className="author">{item.book.authors}</span>
-                      </div>
+                    {/* book img end */}
+                    {/* book info start  */}
+                    <div className="item-info">
+                      <span className="isbn">ISBN {item.book.isbn}</span>
+                      <h5 className="book-title">{item.book.title}</h5>
+                      <span className="author">{item.book.authors}</span>
                     </div>
+                    {/* book info end  */}
                   </div>
                 </div>
+                {/* col start */}
                 <div className="col-lg-7">
+                  {/* row start */}
                   <div className="row align-items-center">
+                    {/* qty box start */}
                     <div className="qty-wrapper col-lg-4">
                       <span className="fw-bold quantity-text">QTY: </span>
                       <div className="qty-box">
@@ -142,50 +153,61 @@ export default function CartDisplay() {
                           onClick={() => handleAddToCart(item.book._id)}
                         ></i>
                       </div>
+                      {/* qty box start */}
                     </div>
-
+                    {/* price start */}
                     <div className="col-lg-6 ">
                       <div className="price-wrapper">
-                        <div className="price-wrapper-content fs-5 me-4">
+                        <div className="fs-5 me-4">
                           <span className="fw-bold">Price:</span>
                           <span> ${item.book.price}</span>
                         </div>
-                        <div className="price-wrapper-content">
-                          <h6 className="fs-5">
-                            <span>
-                              <span className="fw-bold">Subtotal:</span> $
-                              {calcSubtotal(item)}
-                            </span>
-                          </h6>
+
+                        <div className="fs-5">
+                          <span className="fw-bold">Subtotal:</span>
+                          <span> ${calcSubtotal(item)}</span>
                         </div>
                       </div>
+                      {/* price end */}
                     </div>
 
                     <div className="col-lg-2">
+                      {/* remove icon start */}
                       <div className="remove-icon-wrapper d-flex justify-content-center">
                         <i
                           className="bi bi-trash-fill remove-icon"
                           onClick={() => handleRemoveFromCart(item.book._id)}
                         ></i>
+                        {/* remove icon end */}
                       </div>
                     </div>
+                    {/* row 2 end */}
                   </div>
+                  {/* col end*/}
                 </div>
+                {/* row 1 end  */}
               </div>
             ))}
+            {/*cart wrapper end*/}
           </div>
         )}
       </div>
       {cartItems.length > 0 && (
         <div className="container mb-5">
+          {/* checkout wrapper start */}
           <div className="checkout-wrapper ">
+            {/* row start */}
             <div className="row d-flex  ">
+              {/* checkout wrapper left-side start */}
               <div className="col-lg-6 my-5 left-side-wrapper">
                 <h4 className="fw-bold my-5">Shopping Summary</h4>
+                {/* left-side-coupon start */}
                 <div className="my-4 left-side-coupon">
-                  <h6>Have a coupon code?</h6>
+                  <h6 className="coupon-header">Apply (BOOKSHELF) NOW!</h6>
                   <div className="d-flex">
+                    {/*promo-code-wrapper start*/}
                     <div className="promo-code-wrapper">
+                      {/* promo-code-content start */}
                       <div className="promo-code-content">
                         <img
                           src="coupon.png"
@@ -197,45 +219,61 @@ export default function CartDisplay() {
                           className="promo-input"
                           value={couponCode}
                           onChange={(e) => setCouponCode(e.target.value)}
-                          disabled={couponApplied} // Disable input if coupon applied
+                          disabled={couponApplied}
                         />
+                        {/* promo-code-content end */}
                       </div>
+                      {/* promo-code-wrapper end */}
                     </div>
-                    <div>
-                      <i
-                        className="bi bi-arrow-right-square-fill right-arrow"
-                        onClick={applyCoupon}
-                        disabled={couponApplied}
-                      ></i>
-                    </div>
+
+                    <i
+                      className="bi bi-arrow-right-square-fill right-arrow"
+                      onClick={applyCoupon}
+                      disabled={couponApplied}
+                    ></i>
                   </div>
+                  {/* left side coupon end */}
                 </div>
+                {/* checkout wrapper left side end */}
               </div>
               <div className=" col-lg-6 my-5   ">
+                {/* total price wrapper start */}
                 <div className="total-price-wrapper d-flex flex-column">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <span className="fs-5 right-side-text">Subtotal</span>
-                    <span className="fw-bold">${totalPrice}</span>
+                  {/**********************/}
+                  <div className="d-flex justify-content-between align-items-center fs-5">
+                    <span className=" right-side-text">Subtotal</span>
+                    <span className="fw-bold ">${totalPrice}</span>
                   </div>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <span className="fs-5 right-side-text">Tax</span>
-                    <span className="fw-bold">${taxAmount}</span>
+                  {/**********************/}
+                  <div className="d-flex justify-content-between align-items-center fs-5">
+                    <span className=" right-side-text">Tax</span>
+                    <span className="fw-bold ">${taxAmount}</span>
                   </div>
-                  <div className="d-flex justify-content-between align-items-center">
+                  {/**********************/}
+                  <div className="d-flex justify-content-between align-items-center fs-5">
                     <span className="fs-5 right-side-text ">Shipping fees</span>
                     <span className="fw-bold">Free</span>
                   </div>
+                  {/**********************/}
+
                   <hr className="w-100" />
-                  <div className="d-flex justify-content-between align-items-center">
+                  {/**********************/}
+
+                  <div className="d-flex justify-content-between align-items-center fs-5">
                     <span className="fs-5 right-side-text">Total</span>
                     <span className="fw-bold">${calculateTotalPrice()}</span>
                   </div>
+                  {/**********************/}
+
                   <div className="btn-wrapper ">
                     <button className="checkout-btn mt-5 ">Checkout</button>
                   </div>
+                  {/* total price wrapper end */}
                 </div>
               </div>
+              {/* row end */}
             </div>
+            {/* checkout wrapper end */}
           </div>
         </div>
       )}
