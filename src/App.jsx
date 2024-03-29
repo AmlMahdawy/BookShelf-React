79444505bb61787210111b1a309232fa2da66004
Aml Ibrahim (Mully) <amlmahdawy21@gmail.com>
2024-03-29 16:26:50 +0200

    old routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,25 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import LoginPage from "./Pages/LoginPage";
-import RegisterPage from "./Pages/RegisterPage";
-import Home from "./Pages/Home";
-import { AppProvider } from "./Contexts/appContext";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import SystemLayout from "./Layout/SystemLayout";
-import ProtectedRoutes from "./Guard/authGuard";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Home from "./Pages/Home";
 import { AppProvider } from "./Contexts/appContext";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Join from "./Pages/Join";
-
+import Join from "./Pages/Join.jsx"
+import SystemLayout from "./Layout/SystemLayout.jsx";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <SystemLayout />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/home",
-          element: (
-            <ProtectedRoutes>
-              {" "}
-              <Home />{" "}
-            </ProtectedRoutes>
-          ),
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <join />,
-    },
-  ]);
-  const theme = createTheme({
-    breakpoints: {
-      values: {
-        xs: 0,
-        sm: 576,
-        md: 768,
-        lg: 992,
-        xl: 1200,
-        xxl: 1400,
-      },
-    },
-    palette: {
-      book: {
-        main: "#8d27ae",
-      },
-      button: {
-        main: "#8d27ae",
-        hover: "#8d27ae",
-        active: "#651c7d",
-      },
-    },
-  });
+
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <AppProvider>
-          <RouterProvider router={router} />
-        </AppProvider>
-      </ThemeProvider>
       <AppProvider>
-        <BrowserRouter>
-          <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-            <Route path="/home" element={<Home></Home>}></Route>
-            <Route path="/login" element={<Join></Join>}></Route>
-          </Routes>
-        </BrowserRouter>
+      
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home></Home>}></Route>
+              <Route path="/home" element={<Home></Home>}></Route>
+              <Route path="/login" element={<Join></Join>}></Route>
+              
+            </Routes>
+          </BrowserRouter>
+     
       </AppProvider>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
