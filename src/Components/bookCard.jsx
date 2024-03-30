ceb4f5ecc49d0125bc4b0cd5d190465c76b48169
“name“omar <121135630+OmarAbd-Almaksoud@users.noreply.github.com>
2024-03-30 05:14:46 +0200

    last touch

--- src/Components/bookCard.jsx
+++ src/Components/bookCard.jsx
@@ -5,45 +5,47 @@ import { Link } from "react-router-dom";
 
 function BookCard(props) {
   let { props: book } = props;
-  
 
   return (
-    <Link style={{textDecoration:'none'}} to={'/book/'+book._id}>
-    <div>
-      <div
-        className="bookCard d-flex flex-column  justify-content-center align-items-center    p-3"
-        style={{ width: "fit-content" }}
-      >
-        <img
-          className="my-2"
-          src={book.thumbnailUrl}
-          style={{ width: "200px", height: "300px", borderRadius: "15px" }}
-        />
-        <Rating
-          name="simple-controlled "
-          className="mt-2"
-          value={book.rating || 4}
-          readOnly
-        />
+    <Link style={{ textDecoration: "none" }} to={"/book/" + book._id}>
+      <div>
         <div
-          className="category mt-3"
-          style={{ color: "#a757bf", fontSize: "small", fontWeight: "bold" }}
+          className="bookCard d-flex flex-column  justify-content-center align-items-center    p-3"
+          style={{ width: "fit-content" }}
         >
-          {book.categories[0]}
-        </div>
-        <h5 className="title mt-2" style={{ fontWeight: "bold",color:'black' }}>
-          {book.title}
-        </h5>
-        <div className="auther mb-1" style={{ color: "#8f8f8f" }}>
-          {book.authors[0]}
-        </div>
-        <Link style={{textDecoration:"none"}} to={'/book/'+book._id}>
-          <h5 className="price my-4 px-3">{book.price} L.E</h5>
-        </Link>
+          <img
+            className="my-2"
+            src={book.thumbnailUrl}
+            style={{ width: "200px", height: "300px", borderRadius: "15px" }}
+          />
+          <Rating
+            name="simple-controlled "
+            className="mt-2"
+            value={book.rating || 4}
+            readOnly
+          />
+          <div
+            className="category mt-3"
+            style={{ color: "#a757bf", fontSize: "small", fontWeight: "bold" }}
+          >
+            {book.categories[0]}
+          </div>
+          <h5
+            className="title mt-2"
+            style={{ fontWeight: "bold", color: "black" }}
+          >
+            {book.title}
+          </h5>
+          <div className="auther mb-1" style={{ color: "#8f8f8f" }}>
+            {book.authors[0]}
+          </div>
+          <Link style={{ textDecoration: "none" }} to={"/book/" + book._id}>
+            <h5 className="price my-4 px-3">{book.price} $</h5>
+          </Link>
 
-        <div className="rating "></div>
+          <div className="rating "></div>
+        </div>
       </div>
-    </div>
     </Link>
   );
 }
