3b2d5be49dd415dfc852144a3e07f1e281374ed1
Alaa Anees <alaaaneessaad@gmail.com>
2024-03-30 17:10:14 +0200

    composite components

--- src/Contexts/CartContext.jsx
+++ src/Contexts/CartContext.jsx
@@ -1,28 +1,20 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
-
+import { createContext, useEffect, useState } from "react";
+import api from "../Interceptors/Auth";
 const api_url = "http://localhost:3000";
 export const CartContext = createContext();
-
-const axiosInstance = axios.create({
-  baseURL: api_url,
-  headers: {
-    Authorization:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZmM2YTgyODgzNWQzZDhhMzg5OGRkMCIsImVtYWlsIjoiYW1sQGdtYWlsLmNvbSIsImlhdCI6MTcxMTA0MTI5Nn0.DHqQYbw9CWv5z-b_g_XjDy2IY2ATyXR5gXZImxoKVeI",
-  },
-});
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
-  useEffect(() => {
-    fetchCart();
-  }, []);
+  // useEffect(() => {
+  //   fetchCart();
+  // }, []);
 
   const fetchCart = async () => {
     try {
-      const response = await axiosInstance.get("/cart");
+      const response = await api.get(api_url + "/cart");
+
       setCartItems(response.data.cart);
       setTotalPrice(response.data.totalPrice);
     } catch (error) {
@@ -32,7 +24,7 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = async (bookId) => {
     try {
-      await axiosInstance.post("/cart/add-book", { bookId });
+      await api.post(api_url + "/cart/add-book", { bookId });
       fetchCart();
     } catch (error) {
       console.error("Error adding item to cart:", error);
@@ -41,7 +33,7 @@ export const CartProvider = ({ children }) => {
 
   const removeFromCart = async (bookId) => {
     try {
-      await axiosInstance.delete("/cart/remove-book", { data: { bookId } });
+      await api.delete(api_url + "/cart/remove-book", { data: { bookId } });
       fetchCart();
     } catch (error) {
       console.error("Error removing item from cart:", error);
@@ -50,7 +42,7 @@ export const CartProvider = ({ children }) => {
 
   const decrementFromCart = async (bookId) => {
     try {
-      await axiosInstance.patch("/cart/decrement-book", { bookId });
+      await api.patch(api_url + "/cart/decrement-book", { bookId });
       fetchCart();
     } catch (error) {
       console.error("Error decrementing item from cart:", error);
@@ -59,7 +51,7 @@ export const CartProvider = ({ children }) => {
 
   const checkout = async () => {
     try {
-      await axiosInstance.patch("/cart/check-out");
+      await api.patch(api_url + "/cart/check-out");
       fetchCart();
     } catch (error) {
       console.error("Error checking out:", error);
